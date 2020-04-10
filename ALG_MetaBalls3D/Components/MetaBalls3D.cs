41d23081926f78159ff7cadbe4068c55c9db3c56
Albert Li <albertlidesign@gmail.com>
2020-04-10 12:21:53 +0800

    添加项目文件。

---
+++ ALG_MetaBalls3D/Components/MetaBalls3D.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Grasshopper.Kernel.Types;
+using System.Drawing;
+using System.Diagnostics;
+using Grasshopper;
+using Grasshopper.Kernel.Data;
+using System.Linq;
+
+namespace ALG.MetaBalls3D
+{
+    public class ALG_MetaBalls3D : GH_Component
+    {
+        public ALG_MetaBalls3D()
+          : base("MetaBalls3D", "MetaBalls3D", "Extract isosurface from points using marching cubes algorithm on GPU.", "Mesh", "Triangulation") { }
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("Point", "P", "Sample points.", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Boundary", "B", "The scale of the boundingbox's boundary.", GH_ParamAccess.item, 1.1);
+            pManager.AddNumberParameter("VoxelSize", "S", "Voxel Size", GH_ParamAccess.item, 1.0);
+            pManager.AddNumberParameter("Isovalue", "Iso", "Isovalue.", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddMeshParameter("Mesh", "M", "Extract isosurface.", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Time", "T", "time", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            #region input parameters
+            List<Point3d> samplePts = new List<Point3d>();
+            double scale = 1.0;
+            double boundaryRatio = 2.0;
+            double isovalue = 5.0;
+            List<double> time = new List<double>();
+            Stopwatch sw = new Stopwatch();
+
+            DA.GetDataList("Point", samplePts);
+            DA.GetData("Boundary", ref boundaryRatio);
+            DA.GetData("VoxelSize", ref scale);
+            DA.GetData("Isovalue", ref isovalue);
+            #endregion
+
+            #region initialization
+            Box box1 = BasicFunctions.CreateUnionBBoxFromGeometry(samplePts, boundaryRatio);
+
+            Interval xD = box1.X;
+            Interval yD = box1.Y;
+            Interval zD = box1.Z;
+
+            int xCount = (int)Math.Abs(Math.Round(((xD.T1 - xD.T0) / scale), MidpointRounding.AwayFromZero));
+            int yCount = (int)Math.Abs(Math.Round(((yD.T1 - yD.T0) / scale), MidpointRounding.AwayFromZero));
+            int zCount = (int)Math.Abs(Math.Round(((zD.T1 - zD.T0) / scale), MidpointRounding.AwayFromZero));
+
+            Point3d[] a = box1.GetCorners();
+            List<double> b = new List<double>();
+            for (int i = 0; i < 8; i++)
+            {
+                double t = a[i].X + a[i].Y + a[i].Z;
+                b.Add(t);
+            }
+            Point3d baseP = a[b.IndexOf(b.Min())];
+
+            Point3d voxelS = new Point3d(scale, scale, scale);
+
+            var isoSurface = new MetaBalls3D(baseP, xCount, yCount, zCount, voxelS, (float)scale, (float)isovalue, samplePts);
+            #endregion
+
+            sw.Start();
+            int num_activeVoxels = 0, num_Voxels = xCount * yCount * zCount;
+            List<Point3d> resultPts = new List<Point3d>();
+            bool successful = isoSurface.GenMetaBalls(ref resultPts, ref num_activeVoxels);
+
+            if (successful == false)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "No eligible metaballs can be generated, please change isovalue.");
+                return;
+            }
+            this.Message = num_Voxels.ToString();
+            sw.Stop();
+            double tb = sw.Elapsed.TotalMilliseconds;
+            // extract the mesh from result vertices
+
+            sw.Restart();
+            Mesh mesh = BasicFunctions.ExtractMesh(resultPts);
+            //mesh.Faces.CullDegenerateFaces();
+            mesh.FaceNormals.ComputeFaceNormals();
+            mesh.Normals.ComputeNormals();
+
+            sw.Stop();
+            double tc = sw.Elapsed.TotalMilliseconds;
+
+            time.Add(tb);
+            time.Add(tc);
+
+            DA.SetData("Mesh", mesh);
+            DA.SetDataList("Time", time);
+        }
+
+        protected override Bitmap Icon => null;
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{30DCE43A-3775-489A-AFEA-8325DC32F9C5}"); }
+        }
+    }
+}
---
+++ ALG_MetaBalls3D/Funcitons/GenMetaBalls.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace ALG.MetaBalls3D
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct cfloat3
+    {
+        public float x, y, z;
+    }
+    class MetaBalls3D
+    {
+        public Point3d basePoint;
+        public Point3d voxelSize;
+        public int xCount;
+        public int yCount;
+        public int zCount;
+        public float scale;
+        public float isoValue;
+        public List<Point3d> samplePoints;
+
+        public MetaBalls3D() { }
+        public MetaBalls3D(Point3d basePoint, int xCount, int yCount, int zCount, Point3d voxelSize,
+            float scale, float isoValue, List<Point3d> samplePoints)
+        {
+            this.basePoint = basePoint;
+            this.xCount = xCount;
+            this.yCount = yCount;
+            this.zCount = zCount;
+            this.voxelSize = voxelSize;
+            this.scale = scale;
+            this.isoValue = isoValue;
+            this.samplePoints = samplePoints;
+        }
+
+        [DllImport("ALG_MetaBallsDLL.dll", EntryPoint = "CalcMetaBalls")]
+        public static extern bool CalcMetaBalls(cfloat3 bP, cfloat3 vS, int xCount, int yCount, int zCount,
+            float s, float iso, cfloat3[] samplePoints, int sampleCount, ref uint resultLength);
+        [DllImport("ALG_MetaBallsDLL.dll", EntryPoint = "GetResult")]
+        public static extern void GetResult(IntPtr result);
+        public bool GenMetaBalls(ref List<Point3d> vertices, ref int num_activeVoxels)
+        {
+            int sampleCount = samplePoints.Count();
+            cfloat3 bP = ConvertPtToFloat3(basePoint);
+            cfloat3 vS = ConvertPtToFloat3(voxelSize);
+            cfloat3[] smaplePts = new cfloat3[sampleCount];
+            for (int i = 0; i < sampleCount; i++)
+            {
+                smaplePts[i] = ConvertPtToFloat3(samplePoints[i]);
+            }
+
+            uint resultLength = 0;
+            bool successful = CalcMetaBalls(bP, vS, xCount, yCount, zCount, scale, isoValue, smaplePts, sampleCount, ref resultLength);
+
+            if (successful == false)
+            {
+                return successful;
+            }
+            else
+            {
+                int size = Marshal.SizeOf(typeof(cfloat3)) * (int)resultLength;
+                IntPtr result = Marshal.AllocHGlobal(size);
+
+                GetResult(result);
+                Point3d[] pts = new Point3d[(int)resultLength];
+
+                Parallel.For(0, (int)resultLength, i =>
+                {
+                    IntPtr pPointor = new IntPtr(result.ToInt64() + Marshal.SizeOf(typeof(cfloat3)) * i);
+                    pts[i] = ConvertFloat3ToPt((cfloat3)Marshal.PtrToStructure(pPointor, typeof(cfloat3)));
+                });
+                Marshal.FreeHGlobal(result);
+                vertices = pts.ToList();
+                return successful;
+
+            }
+        }
+
+        public cfloat3 ConvertPtToFloat3(Point3d p)
+        {
+            cfloat3 a = new cfloat3();
+            a.x = (float)p.X;
+            a.y = (float)p.Y;
+            a.z = (float)p.Z;
+            return a;
+        }
+        public Point3d ConvertFloat3ToPt(cfloat3 p)
+        {
+            Point3d a = new Point3d();
+            a.X = p.x;
+            a.Y = p.y;
+            a.Z = p.z;
+            return a;
+        }
+    }
+}
